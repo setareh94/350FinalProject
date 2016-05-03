b5efca6ae9a456046c30275fc5269072277c7fe3
Katie <katie@Katies-MacBook-Pro.local>
2016-05-02 22:06:35 -0700

    grass texturing and scooter enabled

---
+++ sceneRendering/sceneRendering/Scooter.c
@@ -0,0 +1,9 @@
+//
+//  Scooter.c
+//  sceneRendering
+//
+//  Created by Katie on 5/2/16.
+//  Copyright © 2016 Setareh Lotfi. All rights reserved.
+//
+
+#include "Scooter.h"
---
+++ sceneRendering/sceneRendering/Scooter.h
@@ -0,0 +1,14 @@
+//
+//  Scooter.h
+//  sceneRendering
+//
+//  Created by Katie on 5/2/16.
+//  Copyright © 2016 Setareh Lotfi. All rights reserved.
+//
+
+#ifndef Scooter_h
+#define Scooter_h
+
+#include <stdio.h>
+
+#endif /* Scooter_h */
