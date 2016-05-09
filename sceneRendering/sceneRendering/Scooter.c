285561ea5453f03a6147cceb7c052c6bf22db792
Katie <katie@Katies-MacBook-Pro.local>
2016-05-09 13:26:25 -0700

    Add scooter files. Empty at this point

--- sceneRendering/sceneRendering/Scooter.c
+++ sceneRendering/sceneRendering/Scooter.c
@@ -2,8 +2,18 @@
 //  Scooter.c
 //  sceneRendering
 //
-//  Created by Katie on 5/2/16.
+//  Created by Katie on 5/1/16.
 //  Copyright © 2016 Setareh Lotfi. All rights reserved.
 //
 
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 #include "Scooter.h"
+
+void drawScooter(float x, float y, float z) {
+    
+    // This is where we will put the scooter once the human is finished
+}
